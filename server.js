496708d3a97575512547acae894d7018821efe9e
aegarmus <aegarmus@gmail.com>
2024-05-25 11:34:01 -0400

    feat: getAllBooks router created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import bookRouter from './src/routes/book.routes.js'
 
 
 import { db } from './src/config/db.config.js'
@@ -10,6 +11,9 @@ dotenv.config()
 
 const app = express()
 
+
+//Middlewares para rutas
+app.use('/api/v1', bookRouter)
 
 
 db()
---
+++ src/routes/book.routes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { getAllBooks } from '../controllers/book.controller.js'
+
+
+const router = express.Router()
+
+router.get('/books', getAllBooks)
+
+
+export default router
